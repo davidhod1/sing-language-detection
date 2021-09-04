e7450619800c806ccd53247338650d93b8a629d1
davidhod1 <davidhodak225@gmail.com>
2021-09-04 17:06:17 +0200

    Updating project files and documentation

--- Project/detection_test.py
+++ Project/detection_test.py
@@ -20,23 +20,25 @@ with mp_holistic.Holistic(min_detection_confidence = 0.7, min_tracking_confidenc
         image, results = mp_integration(frame, holistic)
 
         keypoints = extract_points(results)
+        print(keypoints)
         sequence_prediction.append(keypoints)
         sequence_prediction = sequence_prediction[-60:]
+
 
         if len(sequence_prediction) == 60:
             res = model.predict(np.expand_dims(sequence_prediction, axis=0))[0]
             print(res)
 
-            if (res[np.argmax(res)] > 0.7):
-                prob = res[np.argmax(res)]
-                cv2.putText(image, '{} '.format(sign_actions[np.argmax(res)]), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (255, 255, 255), 2, cv2.LINE_AA)
-                cv2.putText(image, '{} '.format(prob), (20, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
-                            cv2.LINE_AA)
-                print(sign_actions[np.argmax(res)])
-                print(res)
+            if (res[np.argmax(res)] > 0.7 and not np.all(keypoints==0)):
+               prob = res[np.argmax(res)]
+               cv2.putText(image, '{} '.format(sign_actions[np.argmax(res)]), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                             (255, 255, 255), 2, cv2.LINE_AA)
+               cv2.putText(image, '{:.2f} '.format(prob), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                                cv2.LINE_AA)
+               print(sign_actions[np.argmax(res)])
+               print(res)
             else:
-                print("NOne")
+               print("NOne")
 
         cv2.imshow('OpenCv Feed', image)
 
